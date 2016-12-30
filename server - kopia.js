5681936bddbf15b8455867c03eef168831482490
josefmolnar74 <molnar.josef@gmail.com>
2016-12-30 12:09:17 +0100

    lets see if old server works, socket io init disabled

---
+++ server - kopia.js
@@ -0,0 +1,34 @@
+//  OpenShift sample Node application
+var express = require('express'),
+    fs      = require('fs'),
+    app     = express(),
+    eps     = require('ejs'),
+    morgan  = require('morgan');
+
+var server  = require('http').createServer(app),
+    io      = require('socket.io').listen(server);
+
+Object.assign=require('object-assign')
+
+app.engine('html', require('ejs').renderFile);
+app.use(morgan('combined'))
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+app.get('/', function (req, res) {
+  res.render('index_test.html', { pageCountMessage : null});
+});
+
+// error handling
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500).send('Something bad happened!');
+});
+
+io.set('transports', ['websocket']);
+
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+module.exports = app ;
