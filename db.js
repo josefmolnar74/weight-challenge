b271cb248cf998a5484cf4a4dff1d7f2f6d3ec39
josefmolnar74 <molnar.josef@gmail.com>
2016-12-31 01:18:35 +0100

    enabled db connect

--- db.js
+++ db.js
@@ -44,14 +44,23 @@ if (mysqlURL == null && process.env.DATABASE_SERVICE_NAME) {
   }
 }
 
+/*
 var connection = mysql.createConnection({
   host     : mysqlHost,
   user     : mysqlUser,
   password : mysqlPassword,
   port     : mysqlPort,
   database : mysqlDatabase,
-  socketPath: 'mysql-socket-path'
 });
+*/
+
+ var connection = mysql.createConnection({
+  host     : process.env.OPENSHIFT_MYSQL_DB_HOST,
+  user     : process.env.OPENSHIFT_MYSQL_DB_USERNAME,
+  password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
+  port     : process.env.OPENSHIFT_MYSQL_DB_PORT,
+  database : process.env.OPENSHIFT_APP_NAME
+ });
 
 /* Login credentials
 Username: user1NQ
