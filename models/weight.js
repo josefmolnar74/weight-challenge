491dccf61b8b2e6255d396120c54c03837937e52
josefmolnar74 <molnar.josef@gmail.com>
2017-01-02 00:43:43 +0100

    weight support

--- models/weight.js
+++ models/weight.js
@@ -32,7 +32,7 @@ var createWeightData = function(object, callback) {
       } return callback(err);
       console.log('[JOSEF] client query success');
       console.log("[JOSEF] user created with ID " +result.insertId)
-      var resultObject = {weight_ID: result.insertId};
+      var resultObject = {weight_id: result.insertId};
       callback(null, resultObject);
     });
   });
